473684a4f9b1f300edf7d69aa1f56e7a4f670ad5
mirnaiem <mirnaiem8teen@gmail.com>
2024-06-14 17:51:47 +0600

    client comment

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const ticketDB = client.db('ticketDB');
     const eventCollection = ticketDB.collection('eventCollection');
     const bookingCollection = ticketDB.collection('bookingCollection');
